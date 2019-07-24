9bab4f56b5e091955e373e76e88e1952da6b2664
Zachary Mueller <zrm3@students.uwf.edu>
2019-07-24 09:43:02 -0500

    Final Commit

---
+++ HPMA115S0_Python/README.md
@@ -0,0 +1,3 @@
+# HPMA115S0 library in Python
+
+This library is used to interface the HPMA115S0 sensor with a Python script running on a Raspberry Pi.
---
+++ script.py
@@ -0,0 +1,45 @@
+# Air quality monitoring with environmental sensors - testing the sensors
+# Using the following libraries
+#  - Adafruit_Python_BME280 by Adafruit - https://github.com/adafruit/Adafruit_Python_BME280
+#  - HPMA115S0 library in Python by ThingType - https://github.com/ThingType/HPMA115S0_Python_library
+# Tamas Halbrucker, 2018
+
+from HPMA115S0_Python.HPMA115S0 import *
+#from Adafruit_Python_BME280.Adafruit_BME280 import *
+import time
+import csv
+from datetime import datetime
+
+try:
+    print("Aachen Air Quality Monitor v02 starting up...")
+
+    #sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+
+    hpma115S0 = HPMA115S0("/dev/ttyS0")
+    with open('results.csv', mode='w') as results:
+        result_writer = csv.writer(results, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        result_writer.writerow(['Date', 'Time', 'PM2.5 (ug/m3)', 'PM10 (ug/m3)'])
+
+        hpma115S0.init()
+        hpma115S0.startParticleMeasurement()
+
+        while 1:
+            two = []
+            ten = []
+            for i in range(10):
+                if (hpma115S0.readParticleMeasurement()):
+                    
+                    print("PM2.5    = %d ug/m3" % (hpma115S0._pm2_5))
+                    print("PM10     = %d ug/m3" % (hpma115S0._pm10))
+                    two.append(hpma115S0._pm2_5)
+                    ten.append(hpma115S0._pm10)
+                    time.sleep(0.1)
+            val1 = sum(two)/len(two)
+            val2 = sum(ten)/len(ten)
+            result_writer.writerow([datetime.now().date(), datetime.now().time().replace(microsecond=0), val1, val2])
+            print('PM2.5:{val1}')
+            print('PM10:{val2}')
+            time.sleep(1)
+
+except KeyboardInterrupt:
+    print(" Interrupt detected - Exiting.") 
